682a77f131dd9c1f56b6d9f7df9c946d05eff79b
Emmett Hasley <Ehasley1136@jh-mba158-06.d214.org>
2018-10-25 11:24:52 -0500

    Initial Commit

---
+++ FinalGradeCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalGradeCalculator
+//
+//  Created by Emmett Hasley on 10/25/18.
+//  Copyright © 2018 John Heresy High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
