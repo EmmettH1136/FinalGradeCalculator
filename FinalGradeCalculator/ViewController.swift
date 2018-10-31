77947a9598659a795eff399e6b828d5fd5f3c8c7
Emmett Hasley <Ehasley1136@jh-mba158-06.local>
2018-10-31 11:12:21 -0500

    convert to Ipad and also app icon

--- FinalGradeCalculator/ViewController.swift
+++ FinalGradeCalculator/ViewController.swift
@@ -9,16 +9,19 @@
 import UIKit
 
 var grades = [String]()
+var grades2 = [String]()
 let currentColor = UIColor(red: 1.0, green: 185.0/255.0, blue: 145.0/255.0, alpha: 1.0)
 let redColor = UIColor(red: 1.0, green: 112.0/255.0, blue: 83.0/255.0, alpha: 1.0)
 let greenColor = UIColor(red: 146.0/255.0, green: 221.0/255.0, blue: 145.0/255.0, alpha: 1.0)
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UINavigationControllerDelegate {
+    
     
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
     @IBOutlet weak var picker1: UIPickerView!
+    @IBOutlet weak var picker2: UIPickerView!
     @IBOutlet weak var extraCredit: UILabel!
     @IBOutlet weak var result: UILabel!
     
@@ -27,8 +30,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         result.text = ""
         view.backgroundColor = currentColor
         grades = ["A", "B", "C", "D"]
+        grades2 = ["A", "B", "C", "D"]
         self.picker1.dataSource = self
         self.picker1.delegate = self
+        self.picker2.dataSource = self
+        self.picker2.delegate = self
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -75,32 +81,62 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             extraCredit.text = ""
         }
     }
-    func numberOfComponents(in picker1: UIPickerView) -> Int {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    func pickerView(_ picker1: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return grades.count
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1{
+            return grades2.count
+        } else {
+            return grades.count
+        }
     }
-    func pickerView(_ picker1: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return grades[row]
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1{
+            return grades2[row]
+        } else {
+            return grades[row]
+        }
+        
     }
-    func pickerView(_ picker1: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.textField2.text = grades[row]
-        switch textField2.text{
-        case "A":
-            textField2.text = String(90)
-        case .none:
-            print("uh oh")
-        case "B" :
-            textField2.text = String(80)
-        case "C" :
-            textField2.text = String(70)
-        case "D" :
-            textField2.text = String(60)
-        case .some(_):
-            print("Uh idk")
-        default :
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView.tag == 1{
+            self.textField1.text = grades[row]
+            switch textField1.text{
+            case "A":
+                textField1.text = String(90)
+            case .none:
+                print("uh oh")
+            case "B" :
+                textField1.text = String(80)
+            case "C" :
+                textField1.text = String(70)
+            case "D" :
+                textField1.text = String(60)
+            case .some(_):
+                print("Uh idk")
+            default :
                 print("yuhoh")
         }
+        } else {
+            self.textField2.text = grades[row]
+            switch textField2.text{
+            case "A":
+                textField2.text = String(90)
+            case .none:
+                print("uh oh")
+            case "B" :
+                textField2.text = String(80)
+            case "C" :
+                textField2.text = String(70)
+            case "D" :
+                textField2.text = String(60)
+            case .some(_):
+                print("Uh idk")
+            default :
+                print("yuhoh")
+            }
+        }
+        
     }
 }
